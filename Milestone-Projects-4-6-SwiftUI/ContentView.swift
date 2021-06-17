8b7ceab16b700b5d0ea1a3f26370474ac3ac806f
Baris <karalarbaris@gmail.com>
2021-06-17 12:27:40 +0200

    Initial Commit

---
+++ Milestone-Projects-4-6-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Milestone-Projects-4-6-SwiftUI
+//
+//  Created by Baris Karalar on 17.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Milestone-Projects-4-6-SwiftUI/Milestone_Projects_4_6_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_Projects_4_6_SwiftUIApp.swift
+//  Milestone-Projects-4-6-SwiftUI
+//
+//  Created by Baris Karalar on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_Projects_4_6_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
